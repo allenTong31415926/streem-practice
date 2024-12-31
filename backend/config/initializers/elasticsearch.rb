11b6d883565fe3e01e5a044a225444368fbaecff
allenTong31415926 <allen.tong31415926@gmail.com>
2025-01-01 01:13:19 +1100

    Use yml to store elastic auth data

--- backend/config/initializers/elasticsearch.rb
+++ backend/config/initializers/elasticsearch.rb
@@ -1,8 +1,11 @@
 require 'elasticsearch'
 
+# Load the Elasticsearch configuration from the YAML file
+config = YAML.safe_load(ERB.new(File.read(Rails.root.join "config/elasticsearch.yml")).result, aliases: true)[Rails.env]
+
 ElasticsearchClient = Elasticsearch::Client.new(
-  url: 'https://sample.es.streem.com.au',
-  user: 'candidate',
-  password: 'streem',
+  url: config['url'],
+  user: config['user'],
+  password: config['password'],
   log: true # Logs requests and responses for debugging
 )
